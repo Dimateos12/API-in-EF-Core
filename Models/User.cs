a6d1cac5812aa01dd4438e59be7cea43d0d470b5
Mateusz Jabłoński <mateusz.jablonski12@gmail.com>
2022-12-12 22:35:35 +0100

    Create UserModel

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,16 @@
-﻿namespace Projekt.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projekt.Models
 {
     public class User : BaseEntity
     {
+        [Comment("Imię użytkownika")]
         public string Name { get; set; }
+
+        [Comment("Email użytkownika")]
         public string Email { get; set; }
+
+        [Comment("Hasło użytkownika")]
         public string Password { get; set; }
 
     }
