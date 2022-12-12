aa125b77a76ad71d2d19a162cd9b7a93f1ea84b7
Mateusz Jabłoński <mateusz.jablonski12@gmail.com>
2022-12-12 22:35:48 +0100

    Create BaseEntityModel

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -1,13 +1,20 @@
-﻿using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Projekt.Models
 {
     
     public class BaseEntity
     {
+        [Comment("Id użytkownika")]
         public int Id { get; set; }
+
+        [Comment("Status użytkownika")]
         public int Status { get; set; }
+        [Comment("Data dodania")]
         public DateTime DateAdded { get; set; }
+
+        [Comment("Data aktualizacji")]
         public DateTime DateUpdated { get; set; }
 
     }
