d60882396f3c9427afb27a663550bfaa4c16e791
Mateusz Jabłoński <mateusz.jablonski12@gmail.com>
2022-12-12 22:36:49 +0100

    Make Migration

---
+++ Migrations/20221212205246_Recreate Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false, comment: "Id użytkownika")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false, comment: "Status użytkownika"),
+                    DateAdded = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, comment: "Data dodania"),
+                    DateUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, comment: "Data aktualizacji")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
