e432e366374b777809699520b66f04d0bea99183
Mateusz Jabłoński <mateusz.jablonski12@gmail.com>
2022-12-12 22:35:25 +0100

    Create Model of Team

---
+++ Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace Projekt.Models
+{
+    public class Team : BaseEntity
+    {
+        public string  Name { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+    }
+}
